2b7cae6bd2dfefcb498997656c63b2280f50cc18
unknown <faisaljfcl@gmail.com>
2022-09-14 19:51:22 +0600

    Implemented the Relational Data Models

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import MessageThread, Message, Participant
+
+admin.site.register(MessageThread)
+admin.site.register(Message)
+admin.site.register(Participant)
--- base/models.py
+++ base/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class MessageThread(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    message_thread = models.ForeignKey(MessageThread, on_delete=models.CASCADE)
+    body = models.CharField(max_length=500)
+
+
+class Participant(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message_thread = models.ForeignKey(MessageThread, on_delete=models.CASCADE)
+
